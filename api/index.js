bb6cb1b5d8121c489dc89194d0078c0186472c92
Your Name <claudelpierre786@.com>
2024-01-21 13:35:22 -0500

    connect to database

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.Flow)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+
+
