340b427990442d815a7e7d4619663b8a0a4bf3aa
Your Name <claudelpierre786@.com>
2024-02-03 12:45:20 -0500

    updated profile,created listing page and functionalities

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
@@ -35,6 +36,7 @@ app.use(cors());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err,req,res,next) => {
   const statusCode = err.statusCode || 500;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { useRef } from 'react'
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutFailure, signOutStart, signOutSuccess, updateUserFailure, updateUserStart,updateUserSuccess } from '../redux/user/userSlice'
+import { Link } from 'react-router-dom'
 
 export default function Profile () {
   const fileRef = useRef(null);
@@ -156,6 +157,12 @@ export default function Profile () {
         className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
          {loading ? 'Loading...' : 'Update'}
       </button>
+      <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
     </form>
     <div className='flex justify-between mt-5'>
        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
